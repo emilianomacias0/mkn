f29450cf9f02a62bf02dbb116e615d72cb2bde5f
Emiliano Macias <emilianomacias0@gmail.com>
2016-08-18 23:12:14 -0500

    Se agrego squema y tabla para la coleccion Maquinitas

--- collections/collections.js
+++ collections/collections.js
@@ -3,4 +3,51 @@
  */
 Maquinitas = new Mongo.Collection('maquinitas');
 
+Maquinitas.attachSchema(new SimpleSchema({
+	ubicacion:{ type: String, label: "Ubicacion" },
+	tipo:{ type: String, label: "Tipo" },
+	alias:{ type: String, label: "Alias" },
+	denominacion:{ type: Number, label: "Denominacion" },
+	encargado:{ type: String, label: "Encargado",optional:true },
+	telefono:{ type: String, label: "Telefono",optional:true },
+	comision:{ type: Number, label: "Comision" },
+	creadoPor:{ type: String, label: "",optional:true },
+	creadoEl:{ type: Date, label: "" },
+}));
+
 Contadores = new Mongo.Collection('contadores');
+
+
+TabularTables = {};
+
+TabularTables.Contadores = new Tabular.Table({
+  name: "Contadores",
+  collection: Contadores,
+  scrollY:true,
+  columns: [
+  	{data: "alias", title:"Alias"},
+    {data: "fecha", title: "Fecha",render:function(val){return formateaFecha(val)}}, 
+    {data: "contadoresEntrada", title:"Contadores Entrada"},   
+    {data: "contadoresSalida", title:"Contadores Salida"},
+    {data: "totalMoneda", title:"Total monedas"},
+    {data: "totalRecaudado", title:"Total recaudado"},
+    {data: "totalComision", title:"Total comision"},
+    {data: "totalCliente", title:"Total cliente"},
+    {data: "totalNeto", title:"Total Neto"},
+    // {
+    //   data: "lastCheckedOut",
+    //   title: "Last Checkout",
+    //   render: function (val, type, doc) {
+    //     if (val instanceof Date) {
+    //       return moment(val).calendar();
+    //     } else {
+    //       return "Never";
+    //     }
+    //   }
+    // },
+    // {data: "summary", title: "Summary"},
+    // {
+    //   tmpl: Meteor.isClient && Template.bookCheckOutCell
+    // }
+  ]
+});
